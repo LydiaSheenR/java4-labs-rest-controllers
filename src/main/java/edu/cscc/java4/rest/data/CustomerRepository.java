e0b3a121771a59ced73834c98fe00f063fb2b92e
LydiasheenR <lydiatomy@gmail.com>
2019-05-04 18:14:05 -0400

    All tests and postman tests work

--- src/main/java/edu/cscc/java4/rest/data/CustomerRepository.java
+++ src/main/java/edu/cscc/java4/rest/data/CustomerRepository.java
@@ -1,15 +1,17 @@
 package edu.cscc.java4.rest.data;
 
 import edu.cscc.java4.rest.domain.Customer;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
-public interface CustomerRepository {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-  Customer save(Customer entity);
-  Optional<Customer> findById(long primaryKey);
-  Iterable<Customer> findAll();
-  long count();
-  void delete(Customer entity);
-  boolean existsById(long primaryKey);
+//  Customer save(Customer entity);
+//  Optional<Customer> findById(long primaryKey);
+//  Iterable<Customer> findAll();
+//  long count();
+//  void delete(Customer entity);
+//  boolean existsById(long primaryKey);
 
 }
